3be8c377c01c49e54125f36346a1d0d386b87cb6
alexandergustafssonflink <50074799+alexandergustafssonflink@users.noreply.github.com>
2020-10-28 15:47:19 +0100

    fixes for vercel deployment2

--- guldrush/pages/api/handler.js
+++ guldrush/pages/api/handler.js
@@ -1,9 +1,9 @@
 const run = require('./scraper/connection.js')
 
 export default function handler(req, res) {
-  if (req.method === 'GET') {
+//   if (req.method === 'GET') {
       run(); 
-  } else {
-    // Handle any other HTTP method
-  }
+//   } else {
+
+//   }
 }
