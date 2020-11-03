79a369b7acf2beb65588d2da924aa4d3d0fff84d
alexandergustafssonflink <50074799+alexandergustafssonflink@users.noreply.github.com>
2020-11-03 20:08:30 +0100

    added bit of styling

---
+++ guldrush/components/Header/Header.js
@@ -0,0 +1,14 @@
+import styles from "./header.module.css"
+
+const Header = () => {
+
+  return (
+      <div className={styles.mainHeader}>
+       <h1> Guldrush.se </h1>
+      </div>
+      
+   
+  );
+};
+
+export default Header;
--- guldrush/pages/index.js
+++ guldrush/pages/index.js
@@ -4,26 +4,44 @@ import sortPrices from "../functions/sortPrices";
 import { useRouter } from 'next/router'; 
 import Link from 'next/link';
 import styles from "./index.module.css"; 
+import Layout from "../components/Layout/Layout.js"; 
 
 export default function Home({ prices }) {
   return (
+    <>
+    <Layout /> 
+    
     <div className={styles.metalChoice}>
-    <Link href="/guldtackor"> 
+    <h1> Jämför priser på: </h1>
+    <div className={styles.smallButtons}>
+    <Link href="/guld/guldtackor"> 
         <button className={styles.metalButton}> Guldtackor </button>
     </Link>
 
-     <Link href="/silvertackor"> 
+     <Link href="/silver/silvertackor"> 
         <button className={styles.metalButton}> Silvertackor </button>
     </Link>
 
-      <Link href="/guldmynt"> 
+      <Link href="/guld/guldmynt"> 
         <button className={styles.metalButton}> Guldmynt </button>
     </Link>
 
-     <Link href="/silvermynt"> 
+     <Link href="/silver/silvermynt"> 
         <button className={styles.metalButton}> Silvermynt </button>
     </Link>
     </div>
+    <div className={styles.bigButtons}>
+    <Link href="/guld"> 
+        <button className={styles.bigButton}> Alla guldprodukter </button>
+    </Link>
+
+     <Link href="/silver"> 
+        <button className={styles.bigButton}> Alla silverprodukter </button>
+    </Link>
+    </div>
+
+    </div>
+    </>
   )
 }
 
