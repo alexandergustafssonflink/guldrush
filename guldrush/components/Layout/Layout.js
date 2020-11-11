567a5084fcdcd34da5814c075e1d08dfcd63482f
alexandergustafssonflink <50074799+alexandergustafssonflink@users.noreply.github.com>
2020-11-11 15:33:33 +0100

    added some seo

--- guldrush/components/Layout/Layout.js
+++ guldrush/components/Layout/Layout.js
@@ -1,19 +1,78 @@
 import Head from "next/head";
 import Header from "../Header/Header.js";
+import { useRouter } from "next/router";
 
 const Layout = () => {
-  return (
-    <>
-      <Head>
-        <title>Guldrush.se | Jämför priser på ädelmetaller</title>;
-        <link
-          href="https://fonts.googleapis.com/css2?family=Arvo&family=Raleway&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
-      <Header />
-    </>
-  );
+  let router = useRouter();
+  if (router.asPath.includes("guldtackor")) {
+    return (
+      <>
+        <Head>
+          <title>Guldrush.se | Jämför pris på guldtackor</title>;
+          <link
+            href="https://fonts.googleapis.com/css2?family=Arvo&family=Raleway&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <Header />
+      </>
+    );
+  } else if (router.asPath.includes("guldmynt")) {
+    return (
+      <>
+        <Head>
+          <title>Guldrush.se | Jämför pris på guldmynt</title>;
+          <link
+            href="https://fonts.googleapis.com/css2?family=Arvo&family=Raleway&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <Header />
+      </>
+    );
+  } else if (router.asPath.includes("guld")) {
+    return (
+      <>
+        <Head>
+          <title>Guldrush.se | Jämför pris på guld</title>
+          ;
+          <link
+            href="https://fonts.googleapis.com/css2?family=Arvo&family=Raleway&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <Header />
+      </>
+    );
+  } else if (router.asPath.includes("silver")) {
+    return (
+      <>
+        <Head>
+          <title>Guldrush.se | Jämför pris på silver</title>
+          ;
+          <link
+            href="https://fonts.googleapis.com/css2?family=Arvo&family=Raleway&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <Header />
+      </>
+    );
+  } else {
+    return (
+      <>
+        <Head>
+          <title>Guldrush.se | Jämför pris på guld</title>
+          ;
+          <link
+            href="https://fonts.googleapis.com/css2?family=Arvo&family=Raleway&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <Header />
+      </>
+    );
+  }
 };
 
 export default Layout;
