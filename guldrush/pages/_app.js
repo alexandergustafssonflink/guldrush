fd0ccdd98bc8ba66120a9b5dfab32412676a7685
alexandergustafssonflink <50074799+alexandergustafssonflink@users.noreply.github.com>
2020-11-25 20:17:45 +0100

    fixed loadscreen

--- guldrush/pages/_app.js
+++ guldrush/pages/_app.js
@@ -1,9 +1,12 @@
 import styles from "../styles/styles.css";
+import Router from "next/router";
+import NProgress from "nprogress"; //nprogress module
+import "nprogress/nprogress.css"; //styles of nprogress
 
-// export function reportWebVitals(metric) {
-//   console.log(metric);
-// }
-
-export default function MyApp({ Component, pageProps }) {
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
+function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
+export default MyApp;
