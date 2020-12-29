cd3af1b08a6ad26b5a3381c0faef33aecadc61f8
alexandergustafssonflink <50074799+alexandergustafssonflink@users.noreply.github.com>
2020-12-29 19:19:38 +0100

    fixed delete bug?

--- scraper/deleteCollection.js
+++ scraper/deleteCollection.js
@@ -2,7 +2,7 @@ const { MongoClient } = require("mongodb");
 const uri =
   "mongodb+srv://alexander:zni2ev@cluster0.h5faa.mongodb.net/test?retryWrites=true&w=majority";
 // const jsonFile = require("./test.json");
-var fs = require("fs");
+// var fs = require("fs");
 const client = new MongoClient(uri, {
   useUnifiedTopology: true,
 });
