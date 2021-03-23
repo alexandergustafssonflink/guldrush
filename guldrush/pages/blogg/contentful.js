758b3cff6c550df41e64d3d61917f644a9ba38ef
alexandergustafssonflink <50074799+alexandergustafssonflink@users.noreply.github.com>
2021-03-23 14:45:01 +0100

    removed faulty contentful file

--- guldrush/pages/blogg/contentful.js
+++
@@ -1,8 +0,0 @@
-const contentful = require("contentful");
-
-const client = contentful.createClient({
-  space: "wdqa1l12ilb0",
-  accessToken: "Ct_EvA8WqhAh_Zgm8eQgrlGnuzzZZej4TcuwZZQHekM",
-});
-
-export default client;
